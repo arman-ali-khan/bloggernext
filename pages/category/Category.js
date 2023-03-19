aaba4a26d7e52a9de665416d9d68d3553b7627f5
Arman Khan <sparmankhan@gmail.com>
2023-03-19 11:26:37 +0600

    like animation and category post added

--- pages/category/Category.js
+++ pages/category/Category.js
@@ -5,16 +5,16 @@ import { MdOutlineModeComment } from 'react-icons/md';
 import Categories from '../../components/Categories/Categories';
 
 const Category = ({category}) => {
-    console.log(category);
+  
     return (
         <div className='flex md:mx-12 mx-4 gap-4 md:w-9/12 my-2 rounded-lg bg-base-200 px-2 py-2'>
                 <div className='md:w-44 md:h-32 w-24 h-20'>
-                <img className='w-full h-full object-cover rounded-md' src={category.thumb} alt="" />
+                <img className='w-full h-full object-cover rounded-md' src={category?.thumb} alt="" />
                 </div>
                 <div className='w-full'>
                 <div className="text-xs flex justify-between">
               <p>
-                <span>in</span> {category.categories[0]?.label}
+                <span>in</span> {category?.categories[0]?.label}
               </p>
 
               <p className="flex items-center gap-1 font-semibold">
@@ -23,14 +23,14 @@ const Category = ({category}) => {
               </p>
             </div>
             <Link
-              href={`/blog/${category.id}`}
+              href={`/blog/${category?.id}`}
               className={` leading-3 text-base break-words  font-bold`}
             >
-              {category.title.split(" ").slice(0, 13).join(" ")}
-              {category.title.split(" ").length > 13 && "..."}
+              {category?.title.split(" ").slice(0, 13).join(" ")}
+              {category?.title.split(" ").length > 13 && "..."}
             </Link>
-                    {/* <h3 className='text-base md:text-lg'>{category.title}</h3> */}
-                    <p className='hidden md:block'>{category.body.replace(/<\/?[^>]+>/gi, '').split(' ').slice(0,30).join(' ')}</p>
+                    {/* <h3 className='text-base md:text-lg'>{category?.title}</h3> */}
+                    <p className='hidden md:block'>{category?.body.replace(/<\/?[^>]+>/gi, '').split(' ').slice(0,30).join(' ')}</p>
                     <div className="flex justify-between text-xs">
               <p>{moment(category?.date).fromNow()} </p>
               <p className="flex items-center gap-1 font-semibold">
